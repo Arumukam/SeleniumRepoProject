b6c62978c7b19829a6ae9a6ccf4573596bf36216
Rakshitha.AK <Rakshitha.AK@Rakshitha>
2018-05-13 11:33:25 +0530

    test

--- EclipsetoGit/src/Testcheckin3.java
+++ EclipsetoGit/src/Testcheckin3.java
@@ -6,6 +6,7 @@ public class Testcheckin3 {
 		
 		System.out.println("This is auto execution");
 		System.out.println("This is Auto Execition from GIT");	
+		System.out.println("Sample Project execution");
 		
 		
 
